201ef2c38c9aee677cc71d0d2e740c8fecfb590f
yeoldeknob <tmckenna1988@gmail.com>
2022-03-25 11:47:22 -0400

    3/25 Update
    Rewriting all the code so it's more Object Oriented... ugh.

--- scripts.js
+++ scripts.js
@@ -4,8 +4,41 @@ const dealButton = document.getElementById('btn--deal');
 
 // Global variables
 
-const suits = ['spades', 'diamonds', 'clubs', 'hearts'];
-const values = [
+// Contructors
+
+// Game class
+
+class Game {
+    constructor() {
+        this.deal = function () {};
+    }
+}
+
+// Player class
+class Player {
+    constructor(name, cash, hand) {
+        this.name;
+        this.cash;
+        this.hand;
+    }
+}
+
+// Card class
+class Card {
+    constructor(suit, value, imgURL) {
+        this.suit;
+        this.value;
+        this.imgURL;
+    }
+}
+
+// Deck class
+class Deck {
+    constructor(cards) {}
+}
+
+Card.suits = ['spades', 'diamonds', 'clubs', 'hearts'];
+Card.values = [
     'ace',
     '2',
     '3',
@@ -20,170 +53,6 @@ const values = [
     'queen',
     'king',
 ];
-let deck = new Array();
-let players = new Array();
-let currentPlayer;
-
-// Functions
-
-function getDeck() {
-    deck = new Array();
-    for (let i = 0; i < suits.length; i++) {
-        for (let x = 0; x < values.length; x++) {
-            let weight = parseInt(values[x]);
-            if (
-                values[x] === 'jack' ||
-                values[x] === 'queen' ||
-                values[x] === 'king'
-            ) {
-                weight = 10;
-            }
-            if (values[x] === 'ace') {
-                weight = 11;
-            }
-            let card = {
-                Value: values[x],
-                Suit: suits[i],
-                Weight: weight,
-            };
-            deck.push(card);
-        }
-    }
-}
-
-function shuffle(deck) {
-    for (let i = 0; i < 1000; i++) {
-        let location1 = Math.floor(Math.random() * deck.length);
-        let location2 = Math.floor(Math.random() * deck.length);
-        let temp = deck[location1];
-
-        deck[location1] = deck[location2];
-        deck[location2] = temp;
-    }
-}
-
-function createPlayer(num) {
-    players = new Array();
-    for (let i = 1; i <= num; i++) {
-        let hand = new Array();
-        let player = {
-            Name: i === num ? 'Dealer' : 'Player_' + i,
-            ID: i,
-            Points: 0,
-            Hand: hand,
-        };
-        players.push(player);
-    }
-}
-
-function playerUI() {
-    document.getElementById('play--area').innerHTML = '';
-    for (let i = 0; i < players.length; i++) {
-        const div_player = document.createElement('div');
-        const div_playerid = document.createElement('div');
-        const div_hand = document.createElement('div');
-        const div_points = document.createElement('div');
-        const div_status = document.createElement('div');
-
-        div_points.className = 'points';
-        div_points.id = 'points_' + i;
-        div_player.id = 'player_' + i;
-        div_player.className = 'player';
-        div_hand.id = 'hand_' + i;
-        div_hand.className = 'hand';
-        div_status.id = 'status';
-
-        div_playerid.innerHTML = players[i].Name;
-        div_playerid.className = 'playerName';
-
-        div_player.appendChild(div_points);
-        div_player.appendChild(div_status);
-        div_player.appendChild(div_hand);
-        div_player.appendChild(div_playerid);
-        document.getElementById('play--area').appendChild(div_player);
-        if (i === 1) {
-            const btn_hit = document.createElement('button');
-            btn_hit.className = 'button';
-            btn_hit.id = 'btn--hit';
-            btn_hit.innerHTML = 'Hit';
-            document.getElementById('play--area').appendChild(btn_hit);
-            document
-                .getElementById('btn--hit')
-                .setAttribute('onclick', 'hit()');
-        } else {
-            continue;
-        }
-    }
-}
-
-function getCardUI(card) {
-    let div_card = document.createElement('div');
-    let img_card = document.createElement('img');
-    div_card.className = 'card';
-    img_card.className = 'cardImg';
-    img_card.src = `/imgs/${card.Value}_of_${card.Suit}.png`;
-    div_card.appendChild(img_card);
-    return div_card;
-}
-
-function renderCard(card, player) {
-    let hand = document.getElementById('hand_' + player);
-    hand.appendChild(getCardUI(card));
-}
-
-function hit() {
-    let card = deck.pop();
-    players[currentPlayer].Hand.push(card);
-    renderCard(card, currentPlayer);
-    updatePoints();
-    check();
-}
-
-function getPoints(player) {
-    let points = 0;
-    for (let i = 0; i < players[player].Hand.length; i++) {
-        points += players[player].Hand[i].Weight;
-    }
-    players[player].Points = points;
-    return points;
-}
-
-function updatePoints() {
-    for (let i = 0; i < players.length; i++) {
-        getPoints(i);
-        document.getElementById('points_' + i).innerHTML = players[i].Points;
-    }
-}
-
-function check() {
-    if (players[currentPlayer].Points > 21) {
-        document.getElementById('status').innerHTML =
-            players[currentPlayer].Name + ' loses!';
-        document.getElementById('status').style.display = 'inline-block';
-        end();
-    }
-}
-
-function dealHands() {
-    for (let i = 0; i < 2; i++) {
-        for (let x = 0; x < players.length; x++) {
-            let card = deck.pop();
-            players[x].Hand.push(card);
-            renderCard(card, x);
-            updatePoints();
-        }
-    }
-}
-
-function startGame() {
-    currentPlayer = 0;
-    getDeck();
-    shuffle(deck);
-    createPlayer(2);
-    playerUI();
-    dealHands();
-    document.getElementById('player_' + currentPlayer).classList.add('active');
-}
 
 // TEST //
 
