e166caeeb66cad7ffbc129b9be06e8e514ae02c6
yeoldeknob <tmckenna1988@gmail.com>
2022-03-29 10:44:05 -0400

    3/29 Update
    Hit and Stay buttons now have functionality. Not final, obviously.
    
    Added a modal object that will require the players name that will eventually display on the game board.
    
    Will create a modal class to easily create these modal windows later

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-
 // Classes
+
 class Game {
-    constructor () {
+    constructor() {
         this.playing = true;
     }
     renderCardSingle(player, card, faceUp) {
@@ -10,6 +10,7 @@ class Game {
             img_card.src = card.imgURL;
         } else {
             img_card.src = '/imgs/card_back.png';
+            img_card.id = 'card--down';
         }
         img_card.className = 'img--card';
         if (player.name === 'Dealer') {
@@ -25,13 +26,12 @@ class Game {
             } else {
                 this.renderCardSingle(player, player.hand[i], true);
             }
-        }   
+        }
     }
     isOver() {
         this.playing = false;
     }
 }
-
 
 class Player {
     constructor(name, bankroll, active) {
@@ -70,17 +70,34 @@ class Deck {
     constructor() {
         this.cards = [];
         let suit = ['hearts', 'clubs', 'spades', 'diamonds'];
-        let face = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
+        let face = [
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '10',
+            'jack',
+            'queen',
+            'king',
+            'ace',
+        ];
         for (let i = 0; i < suit.length; i++) {
             for (let x = 0; x < face.length; x++) {
                 let value = parseInt(face[x]);
-                if (face[x] === 'jack' || face[x] === 'queen' || face[x] === 'king') {
+                if (
+                    face[x] === 'jack' ||
+                    face[x] === 'queen' ||
+                    face[x] === 'king'
+                ) {
                     value = 10;
                 }
-                if (
-                    face[x] === 'ace'){
-                        value = 11;
-                    }
+                if (face[x] === 'ace') {
+                    value = 11;
+                }
                 let card = new Card(suit[i], face[x], value);
                 this.cards.push(card);
             }
@@ -97,9 +114,9 @@ class Deck {
         }
     }
     dealCards(playerArray) {
-        for (let x = 0; x < playerArray.length; x++){
+        for (let x = 0; x < playerArray.length; x++) {
             for (let i = 0; i < 2; i++) {
-                playerArray[x].hand.push(this.cards.pop());     
+                playerArray[x].hand.push(this.cards.pop());
             }
         }
     }
@@ -112,14 +129,13 @@ class House {
     }
 }
 
-testScoreFunction = function(player) {
-    player.calcPoints();
-    let score = document.getElementById("score--area--number");
-    score.innerHTML = player.points;
-}
+// DOM Elements
 
+let score = document.getElementById('score--area--number');
+const hitButton = document.getElementById('btn--hit');
+const stayButton = document.getElementById('btn--stay');
 
-// // Test Code
+// Game Start
 
 let newGame = new Game();
 const player1 = new Player('Tyler', 100, true);
@@ -127,29 +143,56 @@ const dealer = new Player();
 const house = new House(0);
 let players = [];
 players.push(player1, dealer);
-
 let deck = new Deck();
 deck.shuffle();
 dealer.setDealer();
 player1.bet(50, house);
-
 deck.dealCards(players);
 newGame.renderCards(player1);
 newGame.renderCards(dealer);
-testScoreFunction(player1);
+player1.calcPoints();
+score.innerHTML = player1.points;
+
+// Game functions
+
+hitFunc = function () {
+    let newCard = deck.cards.pop();
+    player1.hand.push(newCard);
+    newGame.renderCardSingle(player1, newCard, true);
+    player1.points += newCard.value;
+    if (player1.points > 21) {
+        score.innerHTML = `${player1.points}! You bust!`;
+    } else {
+        score.innerHTML = player1.points;
+    }
+};
+
+stayFunc = function () {
+    let cardTwo = dealer.hand[1];
+    const cardDown = document.getElementById('card--down');
+    cardDown.src = cardTwo.imgURL;
+    dealer.calcPoints();
+    if (dealer.points < 21) {
+        let newCard = deck.cards.pop();
+        dealer.hand.push(newCard);
+        newGame.renderCardSingle(dealer, newCard, true);
+        dealer.points += newCard.value;
+    } else if (dealer.points === 21) {
+        score.innerHTML = 'Dealer has 21. Dealer wins!';
+    } else {
+        score.innerHTML = 'Dealer busts! You win!';
+    }
+};
+
+// Event Listeners
+
+hitButton.addEventListener('click', hitFunc);
+stayButton.addEventListener('click', stayFunc);
+
+// Console logs
 
 console.log(house);
-console.log(dealer)
+console.log(dealer);
 console.log(player1);
 
-
 // Event loops <-- Read about these
-
-
-// Hit button
-// event listener on the button
-// button runs function
-// function :
-// removes card from deck
-// adds card to player hand
-// updates scores
